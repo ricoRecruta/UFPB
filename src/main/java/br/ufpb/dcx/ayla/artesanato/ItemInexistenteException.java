3c46361cfede74b0854bb78e4ac5e378dac09715
gabriel.felix <gabrielfxgomes@gmail.com>
2024-07-25 11:08:28 -0300

    Obrigado Laisa

---
+++ src/main/java/br/ufpb/dcx/ayla/artesanato/ItemInexistenteException.java
@@ -0,0 +1,8 @@
+package br.ufpb.dcx.ayla.artesanato;
+
+public class ItemInexistenteException extends Exception {
+
+    public ItemInexistenteException(String msg){
+        super(msg);
+    }
+}
---
+++ src/main/java/br/ufpb/dcx/ayla/quiz/Pergunta.java
@@ -0,0 +1,38 @@
+package br.ufpb.dcx.ayla.quiz;
+
+public abstract class Pergunta implements Comparable<Pergunta> {
+    private String enunciado;
+    private String respostaCorreta;
+
+    public Pergunta(String enunciado, String respostaCorreta){
+        this.enunciado = enunciado;
+        this.respostaCorreta = respostaCorreta;
+    }
+
+    public Pergunta(){
+        this("","");
+    }
+
+    public int compareTo(Pergunta pergunta2){
+        return this.enunciado.compareTo(pergunta2.enunciado);
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    public String getRespostaCorreta() {
+        return respostaCorreta;
+    }
+
+    public void setRespostaCorreta(String respostaCorreta) {
+        this.respostaCorreta = respostaCorreta;
+    }
+
+    public abstract boolean estahCorretaResposta(String resposta);
+
+}
