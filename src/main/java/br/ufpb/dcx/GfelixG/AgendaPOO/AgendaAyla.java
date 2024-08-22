80a4b524b089d1df5038bf7bea78b4c012cda4e2
gabriel.felix <gabrielfxgomes@gmail.com>
2024-08-22 11:40:38 -0300

    Arquivos de GUI da agenda

--- src/main/java/br/ufpb/dcx/GfelixG/AgendaPOO/AgendaAyla.java
+++ src/main/java/br/ufpb/dcx/GfelixG/AgendaPOO/AgendaAyla.java
@@ -1,4 +1,45 @@
 package br.ufpb.dcx.GfelixG.AgendaPOO;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.HashMap;
 
-public class AgendaAyla {
+public class AgendaAyla implements Agenda {
+
+    private HashMap<String,Contato> contatos;
+    private GravadorDeDados gravador;
+
+    public AgendaAyla(){
+        this.contatos = new HashMap<>();
+        this.gravador = new GravadorDeDados();
+    }
+
+
+    @Override
+    public boolean cadastraContato(String nome, int dia, int mes) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public Collection<Contato> pesquisaAniversariantes(int dia, int mes) {
+        //TODO
+        return List.of();
+    }
+
+    @Override
+    public boolean removeContato(String nome) throws ContatoInexistenteException {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public void salvarDados() throws IOException {
+        //TODO
+    }
+
+    @Override
+    public void recuperarDados() throws IOException {
+        //TODO
+    }
 }
